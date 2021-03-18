048e804f03b13acd655060caf8a5b1a9fa23f2be
vinceecarter <79314071+vinceecarter@users.noreply.github.com>
2021-03-18 15:44:07 +0800

    Add files via upload

---
+++ BIODATA.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package biodata;
+
+/**
+ *
+ * @author Asus
+ */
+public class BIODATA {
+    //Vince Garcia Code
+    static void Name(String Name) {
+    System.out.println(Name);
+  }
+    static void SchoolName(){
+        System.out.println("Tarlac State University");
+    }
+    static void Campus(){
+        System.out.println("San Isidro Campus");
+    }
+    
+    
+   
+public static void main(String[] args) {
+    //Vince Garcia Code
+    System.out.println("---- User Details ----");
+       Name("Vince Garcia");
+       Name("Michelle Banaag");
+       Name("Mark Ian Donato");
+       
+     System.out.println("---- University ----");
+       SchoolName();
+       
+       System.out.println("---- University ----");
+       Campus();
+       
+    }
+
+}
+  
